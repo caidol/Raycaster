2c9844abae0757572ad3eb25034a72815a6e4fad
Myriad74 <acatchpool@protonmail.com>
2024-03-21 23:06:56 +0000

    Finish raycasting program.

--- src/raycaster.c
+++ src/raycaster.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
+#include <string.h>
 
 #define mapWidth        24
 #define screenWidth     1024
@@ -27,15 +28,27 @@ uint8_t worldMap[mapWidth][mapWidth]=
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,3,3,3,3,3,3,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,3,0,3,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,3,0,0,0,0,3,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,3,0,3,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,3,0,3,3,3,3,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,3,3,3,3,3,3,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
+
+typedef struct {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    bool quit;
+} State;
+
+static State gameState = {
+    .quit = false,
+};
+
+static int currentVerLine = 0;
 
 Vector2 getCurrentRayDirection(Player *player, int currentX){
     /*
@@ -46,11 +59,11 @@ Vector2 getCurrentRayDirection(Player *player, int currentX){
         ->  1 is right side of screen
     */
 
-   float cameraX = 2 * (currentX / screenWidth) - 1;
+   float cameraX = 2 * currentX / (float) screenWidth - 1;
    
    // calculate the ray direction
    float rayDirX = player->dir.x + (player->plane.x * cameraX);
-   float rayDirY = player->dir.x + (player->plane.y * cameraX);
+   float rayDirY = player->dir.y + (player->plane.y * cameraX);
 
    return (Vector2) {.x = rayDirX, .y = rayDirY};
 }
@@ -59,47 +72,159 @@ Vector2 getCurrentRayDirection(Player *player, int currentX){
 calculate the change in distance either for a ray as it traverses one along in x-direction
 or one along in y-direction.
 */
-void calculateDeltaDistances(Vector2 *rayDir){
-    float deltaDistX = (rayDir->x == 0) 
-        ? (rayDir->x = 999999) // set to arbritrarily high number
-        : sqrt(1 + (rayDir->y * rayDir->y / rayDir->x * rayDir->x));
-
-    float deltaDistY = (rayDir->y == 0)
-        ? (rayDir->y = 999999)
-        : sqrt(1 + (rayDir->x * rayDir->x / rayDir->y * rayDir->y));
-
+Vector2 calculateDeltaDistances(Vector2 *rayDir){
+    // set to arbritrarily high number if zero
+    float deltaDistX = (rayDir->x == 0) ? 1e30 : fabsf(sqrt(1 + (rayDir->y * rayDir->y) / (rayDir->x * rayDir->x)));
+    float deltaDistY = (rayDir->y == 0) ? 1e30 : fabsf(sqrt(1 + (rayDir->x * rayDir->x) / (rayDir->y * rayDir->y)));
+    
     return (Vector2) {.x = deltaDistX, .y = deltaDistY};
 }
 
-/* calculate the rays initial intersection with either a horizontal or vertical line */
-void extendRayToFirstHit(Vector2 *deltaDist, Vector2 *mapSquare, Vector2 *rayDir, Player *player){
-    int x = 0;
-
-    // calculate the nearest X and Y distances as a
-    // proportion of the delta X and Y through a square
-
-    float nearestDistX = 0;
-    float nearestDistY = 0;
-
+Vector2I findMapSteps(Vector2 *rayDir){
+    int stepX;
+    int stepY;
+    
     if (rayDir->x < 0){ // the ray vector points to left side of screen
-        float stepX = -1;
-        nearestDistX = (player->pos.x - mapSquare->x) * deltaDist->x;
+        stepX = -1;
     }
     else{
-        float stepX = 1;
-        nearestDistX = (mapSquare->x + 1 - player->pos.x) * deltaDist->x;
+        stepX = 1;
     }
 
     if (rayDir->y < 0){
-        float stepY = -1;
+        stepY = -1;
+    }
+    else{
+        stepY = 1;
+    }
+
+    return (Vector2I) {.x = stepX, .y = stepY};
+}
+
+/* calculate the rays initial intersection with either a horizontal or vertical line */
+Vector2 extendRayToFirstHit(Vector2 *deltaDist, Vector2I *mapSquare, Vector2 *rayDir, Player *player){
+    // calculate the nearest X and Y distances as a
+    // proportion of the delta X and Y through a square
+
+    float nearestDistX;
+    float nearestDistY;
+
+    if (rayDir->x < 0){ // the ray vector points to left side of screen
+        nearestDistX = (player->pos.x - mapSquare->x) * deltaDist->x;
+    }
+    else{
+        nearestDistX = (mapSquare->x + 1.0f - player->pos.x) * deltaDist->x;
+    }
+
+    if (rayDir->y < 0){
         nearestDistY = (player->pos.y - mapSquare->y) * deltaDist->y;
     }
     else{
-        float stepY = 1;
-        nearestDistY = (mapSquare->y + 1 - player->pos.y) * deltaDist->y;
+        nearestDistY = (mapSquare->y + 1.0f - player->pos.y) * deltaDist->y;
     }
 
     return (Vector2) {.x = nearestDistX, .y = nearestDistY};
+}
+
+void drawLineToScreen(Vector2 *deltaDist, Vector2 *nearestDist, Vector2I *mapSquare, int side){
+    colourRGBA colour;
+    // check map square to determine colour
+    switch (worldMap[mapSquare->y][mapSquare->x]){
+        case 1:
+            colour = red;
+            break;
+        case 2:
+            colour = green;
+            break;
+        case 3:
+            colour = blue; 
+            break;
+    }
+
+    float perpWallDist;
+    switch(side){
+        case 0: // east or west
+            perpWallDist = (nearestDist->x - deltaDist->x);
+            break;
+        case 1:
+            perpWallDist = (nearestDist->y - deltaDist->y);
+
+            colour.r /= 2;
+            colour.g /= 2;
+            colour.b /= 2;
+            break;
+    }
+
+    int line_height = (int) (screenHeight / perpWallDist);
+
+    // determine the start and end positions to draw line at
+    int line_start = -line_height / 2 + screenHeight / 2;
+    if (line_start < 0) {line_start = 0;}
+
+    int line_end = line_height / 2 + screenHeight / 2;
+    if (line_end >= screenHeight) {line_end = screenHeight - 1;}
+
+    SDL_SetRenderDrawColor(gameState.renderer, colour.r, colour.g, colour.b, colour.a);
+    SDL_RenderDrawLine(gameState.renderer, currentVerLine, line_start, currentVerLine, line_end);
+}
+
+/* extend the ray out from the player's position, checking until a wall (non-zero) has been hit */
+void raycast(Vector2 *deltaDist, Vector2 *nearestDist, Vector2I *mapSquare, Vector2I *stepDir, Player *player){
+    // Here is where we properly perform the DDA (Digital Differential Analyzer) algorithm
+
+    int side;
+
+    bool hit = false;
+    while (!hit){
+        // check for which of the nearest distances intersects first and update the other one
+        if (nearestDist->x < nearestDist->y){
+            nearestDist->x += deltaDist->x;
+            mapSquare->x += stepDir->x;
+
+            side = 0;
+        }
+        else{
+            nearestDist->y += deltaDist->y;
+            mapSquare->y += stepDir->y;
+
+            side = 1;
+        }
+
+        // check current map square of ray
+        if (worldMap[mapSquare->y][mapSquare->x] > 0){
+            hit = true;
+        }
+    }
+
+    drawLineToScreen(deltaDist, nearestDist, mapSquare, side);
+}
+
+void render(Player player){
+    for (currentVerLine = 0; currentVerLine < screenWidth; currentVerLine++){
+        Vector2 rayDir = getCurrentRayDirection(&player, currentVerLine);
+
+        /*
+        the current map box we're in
+    
+        -> represented as the integer value of our real position to approximate location on worldMap array
+        */
+        Vector2I mapSquare = {
+            .x = (int) player.pos.x,
+            .y = (int) player.pos.y
+        };
+
+        Vector2 deltaDist = calculateDeltaDistances(&rayDir);
+
+        Vector2 nearestDist = extendRayToFirstHit(&deltaDist, &mapSquare, &rayDir, &player);
+        Vector2I stepDir = findMapSteps(&rayDir);
+        
+
+        // perform raycast 
+        raycast(&deltaDist, &nearestDist, &mapSquare, &stepDir, &player);
+
+    }
+
+    currentVerLine = 0;
 }
 
 int main(int argc, char *argv[]){
@@ -111,48 +236,131 @@ int main(int argc, char *argv[]){
 
     //TODO: Create an assert macro to automate error checking on return values of initialisation 
 
-    struct game_state *gameState = NULL;
     // initalise SDL2 window and renderer states
-    gameState->window = SDL_CreateWindow(
+    gameState.window = SDL_CreateWindow(
         "Wolfenstein 3D style raycaster",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         screenWidth, screenHeight, 
-        SDL_WINDOW_ALLOW_HIGHDPI
+        0
     );
 
-    if (gameState->window == NULL){
+    if (!gameState.window){
         printf("Failed to create SDL2 window: %s\n", SDL_GetError());
         exit(1);
     }
 
-    gameState->renderer = SDL_CreateRenderer(
-        gameState->window, 
+    gameState.renderer = SDL_CreateRenderer(
+        gameState.window, 
         -1,
         SDL_RENDERER_PRESENTVSYNC
     );
 
-    if (gameState->renderer == NULL){
+    if (!gameState.renderer){
         printf("Failed to create SDL2 renderer: %s\n", SDL_GetError());
         exit(1);
     }
 
+    SDL_SetHint(SDL_HINT_MOUSE_RELATIVE_MODE_WARP, "1");
+    SDL_SetRelativeMouseMode(true);
+
     // Initialise player struct
-
     Player player = {
-        .pos = {22, 12}, 
-        .dir = {-1, 0}, 
-        .plane = {0, 0.66},
+        .pos = {.x = 22.0f, .y = 12.0f}, 
+        .dir = {.x = -1.0f, .y = 0.0f}, 
+        .plane = {.x = 0.0f, .y = 0.66f},
     };
 
-    /*
-        the current map box we're in
-    
-        -> represented as the integer value of our real position to approximate location on MAP array
-    */
-    Vector2 mapSquare = {
-        .x = (int) player.pos.x,
-        .y = (int) player.pos.y
-    };
+    const float rotateSpeed = 0.025;
+    const float moveSpeed = 0.05;
 
+    gameState.quit = false;
+
+    while (!gameState.quit){
+        SDL_Event event;
+        int mouse_xrel = 0;
+
+        while(SDL_PollEvent(&event)){
+            switch (event.type){
+                case SDL_QUIT:
+                    gameState.quit = true;
+                    break;
+                case SDL_MOUSEMOTION:
+                    mouse_xrel = event.motion.xrel; 
+                    break;
+            }
+        }
+
+        const uint8_t *keystate = SDL_GetKeyboardState(NULL);
+        if (keystate[SDL_SCANCODE_ESCAPE]) {gameState.quit = true;}
+
+        // Apply this rotation matrix below
+        //
+        //  -> [cos x,  -sin x]
+        //  -> [sin x,   cos x]
+        //
+
+        if (mouse_xrel != 0){ // rotate player
+            float rotSpeed = rotateSpeed * (mouse_xrel * -0.1);
+
+            // both camera direction and camera plane must be rotated
+
+            Vector2 oldDir = player.dir;    
+            player.dir.x = player.dir.x * cosf(rotSpeed) - player.dir.y * sinf(rotSpeed);
+            player.dir.y = oldDir.x     * sinf(rotSpeed) + player.dir.y * cosf(rotSpeed);
+
+            Vector2 oldPlane = player.plane;
+            player.plane.x = player.plane.x * cosf(rotSpeed) - player.plane.y * sinf(rotSpeed);
+            player.plane.y = oldPlane.x     * sinf(rotSpeed) + player.plane.y * cosf(rotSpeed);
+        }
+
+        Vector2 deltaPos = {.x = player.dir.x * moveSpeed, .y = player.dir.y * moveSpeed};
+
+        if (keystate[SDL_SCANCODE_W]){ // forwards
+            if (worldMap[(int) player.pos.y][(int) (player.pos.x + deltaPos.x)] == 0){
+                player.pos.x += deltaPos.x;
+            }
+
+            if (worldMap[(int) (player.pos.y + deltaPos.y)][(int) player.pos.x] == 0){
+                player.pos.y += deltaPos.y;
+            }
+        }
+        if (keystate[SDL_SCANCODE_S]){ // backwards
+            if (worldMap[(int) player.pos.y][(int) (player.pos.x - deltaPos.x)] == 0){
+                player.pos.x -= deltaPos.x;
+            }
+
+            if (worldMap[(int) (player.pos.y - deltaPos.y)][(int) player.pos.x] == 0){
+                player.pos.y -= deltaPos.y;
+            }
+        }
+        if (keystate[SDL_SCANCODE_A]){ // strafe left
+            if (worldMap[(int) player.pos.y][(int) (player.pos.x - deltaPos.y)] == 0){
+                player.pos.x -= deltaPos.y;
+            }
+
+            if (worldMap[(int) (player.pos.y - -deltaPos.x)][(int) player.pos.x] == 0){
+                player.pos.y += deltaPos.x;
+            }
+        }
+        if (keystate[SDL_SCANCODE_D]){ // strafe right
+            if (worldMap[(int) player.pos.y][(int) (player.pos.x + deltaPos.y)] == 0){
+                player.pos.x += deltaPos.y;
+            }
+
+            if (worldMap[(int) (player.pos.y - deltaPos.x)][(int) player.pos.x] == 0){
+                player.pos.y -= deltaPos.x;
+            }
+        }
+
+        SDL_SetRenderDrawColor(gameState.renderer, 0x18, 0x18, 0x18, 0xFF);
+        SDL_RenderClear(gameState.renderer);
+
+        render(player);
+
+        SDL_RenderPresent(gameState.renderer);
+    }
+
+    SDL_DestroyRenderer(gameState.renderer);
+    SDL_DestroyWindow(gameState.window);
     return 0;
 }
--- src/raycaster.h
+++ src/raycaster.h
@@ -1,9 +1,19 @@
 #include "..\..\sdl2\i686-w64-mingw32\include\SDL2\SDL.h"
+#include "..\..\sdl2\i686-w64-mingw32\include\SDL2\SDL_events.h"
+#include "..\..\sdl2\i686-w64-mingw32\include\SDL2\SDL_hints.h"
+#include "..\..\sdl2\i686-w64-mingw32\include\SDL2\SDL_render.h"
+#include "..\..\sdl2\i686-w64-mingw32\include\SDL2\SDL_mouse.h"
+#include "..\..\sdl2\i686-w64-mingw32\include\SDL2\SDL_keyboard.h"
+#include "..\..\sdl2\i686-w64-mingw32\include\SDL2\SDL_scancode.h"
 #undef main
 
 typedef struct{
-    float x; float y;
+    float x, y;
 } Vector2;
+
+typedef struct{
+    int x, y;
+} Vector2I;
 
 typedef struct{
     Vector2 pos;
@@ -11,12 +21,30 @@ typedef struct{
     Vector2 plane;
 } Player;
 
+/*
 struct game_state{
     SDL_Window *window;
     SDL_Renderer *renderer;
 };
+*/
+
+typedef struct{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+} colourRGBA;
+
+colourRGBA red = {.r = 0xFF, .g = 0x00, .b = 0x00, .a = 0xFF};
+colourRGBA green = {.r = 0x00, .g = 0xFF, .b = 0x00, .a = 0xFF};
+colourRGBA blue = {.r = 0x00, .g = 0x00, .b = 0xFF, .a = 0xFF};
+
+//static struct game_state *gameState = NULL;
+//static int currentVerLine;
 
 Vector2 findRayDirection(Player *player, int currentX);
-void findNextXStep();
-void findNextYStep();
-void extendRayToFirstHit();
+Vector2 extendRayToFirstHit(Vector2 *deltaDist, Vector2I *mapSquare, Vector2 *rayDir, Player *player);
+Vector2I findMapSteps(Vector2 *rayDir);
+Vector2 calculateDeltaDistances(Vector2 *rayDir);
+void raycast();
+void render();
